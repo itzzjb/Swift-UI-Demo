54dc23da3a4560c3d5e683f327bbfbb6b993c1af
Januda Bethmin <desilvabethmin@gmail.com>
2024-04-02 10:56:52 +0530

    changing the basic ui

--- SwiftUIDemo/ContentView.swift
+++ SwiftUIDemo/ContentView.swift
@@ -7,18 +7,36 @@
 
 import SwiftUI
 
+// ContentView structure is inheriting a structure called View
 struct ContentView: View {
+    // Body is defined as a variable
+    // some -> It says it is LIKE a structure called View
     var body: some View {
+        // Vertical Stack (Like column in Android)
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            // Horizontal Stack (Like row in Android)
+            HStack {
+                
+                Text("Welcome to my world!")
+                    .font(.headline) // Changing the font style into a pre-defined headline font.
+                    // When we use predefined stuff it is easy for us to do changes for whole application at once.
+                
+                // We are using a systemName to find a predefined icons of iOS
+                Image(systemName: "heart.fill")
+                    .foregroundColor(.blue) // Changing the foregrounf color of the text
+                    .imageScale(.large) // Changing the size of the image
+                    .foregroundStyle(.tint)
+                
+            }.padding(20) // Here we overiding the padding size to 20
+
         }
+        // Modifiers are added after the View with a . at the start.
+        // Here we adding the default padding
         .padding()
     }
 }
 
+// Generating a preview of the ContentView() view.
 #Preview {
     ContentView()
 }
--- SwiftUIDemo/SwiftUIDemoApp.swift
+++ SwiftUIDemo/SwiftUIDemoApp.swift
@@ -5,12 +5,24 @@
 //  Created by Januda Bethmin  on 2024-04-02.
 //
 
+
+// ...App.swift
+// This is kind of like the main enterpoint of the application.
+
 import SwiftUI
 
+// @main -> Says iOS at the runtime that it's gonna be the main execution of the application
 @main
+// Like the concept of classes in Kotlin here we have the concept of structues. (struct)
+// It's inheriting a structure called App ( Comes with SwiftUI import )
 struct SwiftUIDemoApp: App {
+    // Body is defined as a variable
+    // some -> It says it is LIKE a structure called Scene
     var body: some Scene {
+        // WindowGroup {} -> Used to display something
         WindowGroup {
+            // Whatever we need to show is here
+            // Normally calls a structure that is in another .. .swift file.
             ContentView()
         }
     }
